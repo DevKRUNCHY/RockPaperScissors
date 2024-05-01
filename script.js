c30ceb806e24d66e9a5d90f3d234ce39440b0ab8
KRUNCHY <119650906+DevKRUNCHY@users.noreply.github.com>
2024-05-01 17:09:31 -0500

    Added player input
    Got the choice to translate to a number between 0-2. Also got scores got up.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+let playerInput = prompt("What do you choose? Rock, Paper, or Scissors?")
+let playerScore = 0
+let aiScore = 0
+
 function getComputerChoice() {
     let aiInput = Math.floor(Math.random() * 3);
     console.log(aiInput)
 }
+
+ function getPlayerChoice() {
+    if (playerInput = "Rock") {
+        playerInput = 0
+        console.log(playerInput)
+    } else if (playerInput = "Paper") {
+        playerInput = 1
+        console.log(playerInput)
+    } else if (playerInput = "Scissors") {
+        playerInput = 2
+        console.log(playerInput)
+ }
+
+getPlayerChoice()
 
 getComputerChoice()
