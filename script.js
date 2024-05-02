d26f6a7307635760342eee8d8e8667fc85ea5927
KRUNCHY <119650906+DevKRUNCHY@users.noreply.github.com>
2024-05-02 02:09:01 -0500

    Finished Game
    Finished the js of RPS with looping the game how many times you want to.

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-let playerInput = prompt("What do you choose? Rock, Paper, or Scissors?")
-const playerSelection = getPlayerChoice()
-const aiSelection = getComputerChoice()
 let playerScore = 0
 let aiScore = 0
+
+function getPlayerChoice() {
+    let playerInput = prompt("What do you choose? Rock, Paper, or Scissors?")
+    if (playerInput == "Rock") {
+        playerInput = 0
+    } else if (playerInput == "Paper") {
+        playerInput = 1
+    } else if (playerInput == "Scissors") {
+        playerInput = 2
+    }
+    return(playerInput)
+}
 
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getPlayerChoice() {
-    if (playerInput = "Rock") {
-        playerInput = 0
-        console.log(playerInput)
-    } else if (playerInput = "Paper") {
-        playerInput = 1
-        console.log(playerInput)
-    } else if (playerInput = "Scissors") {
-        playerInput = 2
-        console.log(playerInput)
-    }
-    return(playerInput)
-}
 
-console.log(aiSelection)
+function playRound() {
 
-function playRound(playerChoice, aiChoice) {
+    playerChoice = getPlayerChoice()
+    aiChoice = getComputerChoice() 
+
     if (playerChoice === aiChoice) {
         console.log("Its a tie!")
-    } 
+    } else if (playerChoice == 2 && aiChoice == 0) {
+        console.log("You Lose! Scissors beats rock.")
+        aiScore += 1
+    } else if (playerChoice == 0 && aiChoice == 2) {
+        console.log("You Win! Rock beats Scissors.")
+        playerScore += 1
+    } else if (playerChoice < aiChoice) {
+        console.log("You Lose!")
+        aiScore += 1
+    } else if (playerChoice > aiChoice) {
+        console.log("You Won!")
+        playerScore += 1
+    }
+    console.log(`Player: ${playerScore}`)
+    console.log(`Bot: ${aiScore}`)
 }
 
-playRound(playerSelection, aiSelection)
+function playGame(amount) {
+    for (let rounds = 0; rounds < amount; rounds++) {
+        playRound()
+    }
+}
+
+playGame(5)
+
+
